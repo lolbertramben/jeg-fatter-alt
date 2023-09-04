c1cc49b4042bbf4df16edf8acfb90a26b034cca0
Benjamin <55606@dmjx.dk>
2023-09-04 12:20:06 +0200

    link optimeret

--- script.js
+++ script.js
@@ -1,56 +1,35 @@
+
 class Link {
-    constructor(link, text, section){
-        this.createLink = document.createElement("a");
-        this.createLink.href = link;
-        this.createLink.innerText = text;
-        this.br = document.createElement("br");
-        section.append(this.createLink, this.br);
+    constructor(urls, texts){
+        this.linkEl = document.createElement("a");
+        this.linkEl.href = urls;
+        this.linkEl.innerText = texts;
+    };
+    update(){
+        this.li = document.createElement("li")
+        this.li.append(this.linkEl)
+        this.links = document.querySelector("#links");
+        this.links.append(this.li);
     };
 };
 
-//Sections
-const body = document.body;
-const header = document.querySelector("#header")
-const ikkeBesøgt = document.querySelector("#ikkeBesøgt");
-const besøgtTitel = document.querySelector("#besøgtTitel");
-const besøgt = document.querySelector("#besøgt");
+class Element {
+    constructor(){
+        
+    };
+};
 
-//Overskrift
-const overskrift = document.createElement("h1");
-overskrift.innerText = "Her er lidt om Kalaha";
-header.append(overskrift)
+let links = [];
+let Elements = [];
 
-//Besøgt
-const besøgtTitelText = document.createElement("h1");
-besøgtTitelText.innerText = "Besøgte links";
-besøgtTitel.append(besøgtTitelText);
+let urls =["https://da.wikipedia.org/wiki/Kalaha", "https://www.br.dk/blog/kalaha-regler/", "https://www.bog-ide.dk/produkt/115216/kalaha-dize", "https://www.coolshop.dk/legetoej/braetspil-og-spil/kalaha/", "https://kalahamusic.dk/"];
+let texts = ["Link til Wikipwdia.org", "Link til regler", "Link til webshop 1", "Link til webshop 2", 'Link til bandet "Kalaha"'];
 
-//Link 1
-const kalahaWiki = "https://da.wikipedia.org/wiki/Kalaha";
-const wikiText = "Link til Wikipwdia.org";
-new Link(kalahaWiki, wikiText, ikkeBesøgt);
+for (let i = 0; i < urls.length; i++){
+    let newLink = new Link(urls[i], texts[i]);
+    links.push(newLink);
+};
 
-//Link 2
-const kalahaRegler = "https://www.br.dk/blog/kalaha-regler/";
-const reglerText = "Link til regler";
-new Link(kalahaRegler, reglerText, ikkeBesøgt);
-
-//Link 3
-const webShop1 = "https://www.bog-ide.dk/produkt/115216/kalaha-dize";
-const webShopText1 = "Link til webshop 1";
-new Link(webShop1, webShopText1, ikkeBesøgt);
-
-//Link 4
-const webShop2 = "https://www.coolshop.dk/legetoej/braetspil-og-spil/kalaha/";
-const webShopText2 = "Link til webshop 2";
-new Link(webShop2, webShopText2, ikkeBesøgt);
-
-//Link 5
-const kalahaBand = "https://kalahamusic.dk/";
-const kalahaBandText = 'Link til bandet "Kalaha"';
-new Link(kalahaBand, kalahaBandText, ikkeBesøgt);
-
-
-//new Link (123, "test", ikkeBesøgt);
-
-
+links.forEach(Link => {
+    Link.update();
+});
