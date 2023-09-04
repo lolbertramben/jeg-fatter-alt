870b5a41547180bce7adf722ee042d4a9a496f5c
Benjamin <55606@dmjx.dk>
2023-09-04 14:14:58 +0200

    add links

--- script.js
+++ script.js
@@ -30,11 +30,23 @@ elements.push(new Element("https://www.bog-ide.dk/produkt/115216/kalaha-dize", "
 elements.push(new Element("https://www.coolshop.dk/legetoej/braetspil-og-spil/kalaha/", "Link til webshop 2"));
 elements.push(new Element("https://kalahamusic.dk/", 'Link til bandet "Kalaha"'));
 
+
+
 for (let i = 0; i < elements.length; i++){
-    let newLink = new Link(elements[i], elements[i]);
+    let newLink = new Link(elements[i]);
     links.push(newLink);
 };
 
 links.forEach(Link => {
     Link.update();
 });
+
+function addLink(){
+    let urlInput = document.querySelector("#urlLink").value;
+    let titleInput = document.querySelector("#titleLink").value;
+    elements.unshift(new Element(urlInput, titleInput));
+    links.unshift(new Link(elements[0]));
+    links[0].update();
+    document.querySelector("#urlLink").value = "";
+    document.querySelector("#titleLink").value = "";
+};
