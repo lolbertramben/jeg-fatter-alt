3bfe8d46fada5079c8a8e22b653db714332331f6
Benjamin <benjamin.mads.andresengmail.com>
2023-09-06 15:42:39 +0200

    Comments added

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 class Link {
+    // Make anchor element with url and text
     constructor(elements){
         this.linkEl = document.createElement("a");
         this.linkEl.href = elements.url;
         this.linkEl.setAttribute("target", "_selfe");
         this.linkEl.innerText = elements.text;
     };
+
+    // Append anchor element to li element and append to HTML
     update(){
         this.li = document.createElement("li")
         this.li.append(this.linkEl)
@@ -13,10 +16,13 @@ class Link {
     };
 };
 
+// Array for link objects HTML-ready
 let links = [];
+
+// Array for hardcoded data
 let elements = [];
 
-//Links
+// Hardcoded data 
 elements.push({url: "123", text: "test"});
 elements.push({url: "https://da.wikipedia.org/wiki/Kalaha", text: "Link til Wikipwdia.org"});
 elements.push({url: "https://www.br.dk/blog/kalaha-regler/", text: "Link til regler"});
@@ -25,16 +31,18 @@ elements.push({url: "https://www.coolshop.dk/legetoej/braetspil-og-spil/kalaha/"
 elements.push({url: "https://kalahamusic.dk/", text: 'Link til bandet "Kalaha"'});
 
 
-
+// Makes HTML elements for every object in elements
 for (let i = 0; i < elements.length; i++){
     let newLink = new Link(elements[i]);
     links.push(newLink);
 };
 
+// Puts HTML elements on website
 links.forEach(Link => {
     Link.update();
 });
 
+// Takes input from website and makes a new anchor and li element and puts it on thw website
 function addLink(){
     let urlInput = document.querySelector("#urlLink").value;
     let titleInput = document.querySelector("#titleLink").value;
